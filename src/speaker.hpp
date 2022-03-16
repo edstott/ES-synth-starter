c7ef7a355de3e0b2246930faa58f89172b9ec583
inigo-selwood <inigo.selwood@outlook.com>
2022-03-16 14:55:21 +0000

    Added waveform functions

--- src/speaker.hpp
+++ src/speaker.hpp
@@ -11,7 +11,6 @@ enum Waveform {
     SAWTOOTH,
     SQUARE,
     TRIANGLE,
-    SINE,
 };
 
 enum Key {
@@ -34,29 +33,179 @@ enum Key {
     B       = 2,
 };
 
-static Waveform waveform = Waveform::SAWTOOTH;
+struct Note {
+
+public:
+
+    Key key;
+
+    int8_t octave;
+
+    Note() : key(Key::C), octave(4) {}
+
+    Note(const Key &key, const int8_t &octave) {
+        this->key = key;
+        this->octave = octave;
+    }
+
+};
 
 static const uint16_t updatePeriod = 22000;
 
-static uint32_t pitch = 0;
+static uint16_t pitches[6];
 
-uint32_t keyPitch(const Key &key, const uint8_t &octave) {
+Waveform waveform = Waveform::SAWTOOTH;
+
+/* Generates a sawtooth waveform
+
+Arguments
+---------
+pitch: const uint32_t &
+    the pitch (frequency) of the waveform
+time: const uint32_t &
+    the current time since initialization in microseconds
+
+Returns
+-------
+amplitude: uint8_t
+    the corresponding output amplitude
+*/
+uint8_t generateSawtooth(const uint32_t &pitch, const uint32_t &time) {
+	const uint32_t period = 1e6 / pitch;
+	const uint32_t phase = time % period;
+	return (phase << 7) / period + 127;
+}
+
+/* Generates a square waveform
+
+Arguments
+---------
+pitch: const uint32_t &
+    the pitch (frequency) of the waveform
+time: const uint32_t &
+    the current time since initialization in microseconds
+
+Returns
+-------
+amplitude: uint8_t
+    the corresponding output amplitude
+*/
+uint8_t generateSquare(const uint32_t &pitch, const uint32_t &time) {
+	const uint32_t period = 1e6 / pitch;
+	const uint32_t phase = time % period;
+    return (phase < period / 2) ? 0 : 255;
+}
+
+/* Generates a triangle waveform
+
+Arguments
+---------
+pitch: const uint32_t &
+    the pitch (frequency) of the waveform
+time: const uint32_t &
+    the current time since initialization in microseconds
+
+Returns
+-------
+amplitude: uint8_t
+    the corresponding output amplitude
+*/
+uint8_t generateTriangle(const uint32_t &pitch, const uint32_t &time) {
+	const uint32_t period = 1e6 / pitch;
+	const uint32_t phase = time % period;
+	uint32_t result = (phase << 9) / period;
+    return (phase > (period / 2)) ? 512 - result : result;
+}
+
+/* Scales a signal to a given volume range
+
+Arguments
+---------
+value: const uint8_t &
+    the value whose volume to scale, presuming it's in the range (0, 256]
+volume: const uint8_t &
+    the volume level to which to scale, with 255 representing no change, and
+    0 being silent
+
+Returns
+-------
+amplitude: uint8_t
+    the value scaled to the given volume
+*/
+uint8_t scaleVolume(const uint8_t &value, const uint8_t &volume) {
+    const int16_t delta = value - 127;
+    const int16_t adjust = (delta * volume) >> 8;
+    return 127 + adjust;
+}
+
+/* Mixes a number of signal channels together
+
+Uses the headroom paradigm to ensure all the channels (when summed) fit in the
+maximum range. Basically a summed average
+
+Arguments
+---------
+channels: const uint8_t *
+    pointer to an array of amplitude values
+channelCount: const uint8_t &
+    the number of channels in the array
+
+Returns
+-------
+amplitude: uint8_t
+    the amplitude of the resulting mixed signal
+*/
+uint8_t mix(const uint8_t *channels, const uint8_t &channelCount) {
+    uint16_t result = 0;
+    for(uint8_t index = 0; index < channelCount; index += 1)
+        result += channels[index];
+    return result / channelCount;
+}
+
+/* Gets the pitch of a given key
+
+Uses some ~fancy~ math to evaluate the pitch of the key, relative to A4=440Hz.
+Becomes less precise, the further away the given key is from A4.
+
+Uses fixed-point arithmetic with a "precision" of 3 decimal places.
+
+Arguments
+---------
+key: const Key &
+    the key of the note
+octave: const uint8_t
+    the octave the note is in
+
+Returns
+-------
+pitch: uint16_t
+    the pitch of the note, in Hertz
+*/
+uint16_t keyPitch(const Key &key, const uint8_t octave = 4) {
     uint32_t result = 440;
 
     const int8_t octaveDelta = octave - 4;
-    int16_t steps = key + octaveDelta * 11;
+    int16_t steps = key + (octaveDelta * 11);
 
     const bool positive = steps > 0;
-    steps = abs(steps);
+    steps = (steps > 0) ? steps : -steps;
 
+    result <<= 3;
     for(uint8_t step = 0; step < steps; step += 1) {
         result *= positive ? 1059463 : 1e6;
         result /= positive ? 1e6 : 1059463;
     }
+    result >>= 3;
 
     return result;
 }
 
+/* Audio ISR
+
+Interrupt service routine which mixes the channels of each note being played,
+scales the result to the current volume level, and writes the result to the
+on-board speaker
+*/
 void updateRoutine() {
     static uint32_t microseconds = 0;
     microseconds += updatePeriod;
@@ -78,6 +227,10 @@ void updateRoutine() {
     }
 }
 
+/* Initializes speaker
+
+Configures ports and registers ISR
+*/
 void initialize() {
     pinMode(Pins::LEFT, OUTPUT);
     pinMode(Pins::RIGHT, OUTPUT);
